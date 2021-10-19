3f6cb4fad07b4853880a7a1faf157806544c35e6
Thomas <tchalifour91@gmail.com>
2021-10-19 08:31:15 -0400

    norminette update

--- cursus_projets/libft/ft_strmapi.c
+++ cursus_projets/libft/ft_strmapi.c
@@ -1,15 +1,3 @@
-/* 1. La fonction cree une chaine de char. La chaine est initialisee
- * avec malloc et prend la grandeur de la chaine s + 1 pour le '\0'.
- * 2. Cette meme chaine egale la valeur retournee de la chaine
- * passer en parametre de la fonction mystere f.
- * 3. Puis a chaine rst retournee
- *
- * Dans la foncition une protection pour malloc et si
- * la chaine s est vide est de mis */
-
-/* La fonction va comme suit. Une chaine est cree pour le retour de valeur de la fonction ft_strmapi.
- * Puis, une variable de comptage est initialiser a 0 pour defiler la chiane s1 passer en
- * parametre de la fonction f. Ensuite la */
 #include "libft.h"
 
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
--- cursus_projets/libft/ft_strtrim.c
+++ cursus_projets/libft/ft_strtrim.c
@@ -3,33 +3,31 @@
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-    char    *s2;
-    size_t  size;
+	char	*s2;
+	size_t	size;
 
-    size = 0;
-    if (!s1)
-        return (NULL);
-    if (!set)
-        return (NULL);
-    while (*s1 && ft_strchr(set, *s1))
-        s1++;
-    size = ft_strlen(s1);
-    while (size && ft_strchr(set, s1[size]))
-        size--;
-    s2 = ft_substr(s1, 0, size + 1);
-    return (s2);
+	size = 0;
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	size = ft_strlen(s1);
+	while (size && ft_strchr(set, s1[size]))
+		size--;
+	s2 = ft_substr(s1, 0, size + 1);
+	return (s2);
 }
 
-/*
 int main(void)
 {
-    char    *s2;
-    char    *s1 = "";
-    char    *set = "";
+	char	*s2;
+	char	*s1 = "";
+	char	*set = "";
 
-    s2 = ft_strtrim(s1, set);
-    printf("Comparaison %d\n", strcmp(s2, ""));
-    printf("%s\n", s2);
-    return (0);
+	s2 = ft_strtrim(s1, set);
+	printf("Comparaison %d\n", strcmp(s2, ""));
+	printf("%s\n", s2);
+	return (0);
 }
-*/
